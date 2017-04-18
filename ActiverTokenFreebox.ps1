15353c2acc302df4e07bc3c5221a24a9aac6d6ae
Isa <isabelle.riverain@gmail.com>
2017-04-18 20:14:20 +0200

    First draft
    Compare Freebox video files to local files

---
+++ ActiverTokenFreebox.ps1
@@ -0,0 +1,31 @@
+﻿
+$FbxApi = Invoke-RestMethod -Uri http://mafreebox.freebox.fr/api_version
+
+# Déclarer l'application            
+$AuthJson = @'
+{
+   "app_id": "fr.freebox.testapp",
+   "app_name": "Test App",
+   "app_version": "1.0.0",
+   "device_name": "Mon PC"
+}
+'@            
+            
+$BaseURL = "http://mafreebox.freebox.fr$($FbxApi.api_base_url)v$([int]$FbxApi.api_version)"            
+            
+# Demander l'autorisation            
+$post = Invoke-RestMethod -Uri "$BaseURL/login/authorize" -Method Post -Body $AuthJson
+
+$statusToken = Invoke-RestMethod -Uri "$BaseURL/login/authorize/$($post.result.track_id)"            
+# Requete GET pour interroger le statut            
+while ($statusToken.result.status -eq "pending") {            
+ $statusToken = Invoke-RestMethod -Uri "$BaseURL/login/authorize/$($post.result.track_id)"            
+ Start-Sleep -Seconds 1            
+}            
+            
+if ($statusToken.result.status -eq "granted") {            
+    "Bravo, application autorisée"            
+    "le app_token secret à conserver est: {0}" -f $post.result.app_token            
+} else {            
+    "Echec: résultat du processus d'autorisation: {0}" -f $statusToken.result.status            
+}   
---
+++ SupprimerTelechargementEnTrop.ps1
@@ -0,0 +1,36 @@
+﻿$AppToken= 'yourToken'            
+$Challenge = (Invoke-RestMethod -Uri "$BaseURL/login").result.challenge            
+            
+# password = hmac-sha1(app_token, challenge)            
+# http://leftshore.wordpress.com/2010/10/04/hmac-sha1-using-powershell/            
+$hmacsha = New-Object System.Security.Cryptography.HMACSHA1            
+$hmacsha.key = [Text.Encoding]::ASCII.GetBytes($AppToken)            
+$signature = $hmacsha.ComputeHash([Text.Encoding]::ASCII.GetBytes($Challenge))            
+$password = [string]::join("", ($signature | % {([int]$_).toString('x2')}))            
+            
+$SessionJson = @"
+{
+   `"app_id`": `"fr.freebox.testapp`",
+   `"password`": `"$($password)`"
+}
+"@            
+            
+$session = Invoke-RestMethod -Uri "$BaseURL/login/session/" -Method Post -Body $SessionJson            
+            
+'ouverture de la session avec succes: {0}' -f $session.success             
+#'le session_token est: {0}' -f $session.result.session_token            
+## Afficher les permissions            
+#$session.result.permissions
+
+$Header = @{'X-Fbx-App-Auth' = $($session.result.session_token)}            
+# Afficher le journal d'appel            
+#(Invoke-RestMethod -Uri "$BaseURL/call/log/" -Headers $Header).result | ft -AutoSize
+$FreeboxVideao = (Invoke-RestMethod -Uri "$BaseURL/fs/ls/L0Rpc3F1ZSBkdXIvVmlkw6lvcw==" -Headers $Header).result.Name | ft -AutoSize
+
+$telechargementItems = Get-ChildItem C:\Users\isabe\Downloads -Recurse | Select-Object Name
+
+ForEach($item in $telechargementItems)  {
+    if ($FreeboxVideao -contains $item) {
+        ($item.Name)
+    }
+}
